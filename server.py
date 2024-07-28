9ba69dc88171a4e09810a390c70c3632a20880f6
Viktoriya <75164029+vika1923@users.noreply.github.com>
2024-07-28 13:39:26 +0500

    Add files via upload

--- server.py
+++ server.py
@@ -12,37 +12,37 @@ app, socket = create_app()
 # COMMUNICATION ARCHITECTURE
 
 # Join-chat event. Emit online message to other users and join the room
-# @socket.on("join-chat")
-# def join_private_chat(data):
-#     print()
-#     room = data["rid"]
-#     join_room(room=room)
-#     socket.emit(
-#         "joined-chat",
-#         {"msg": f"{room} is now online."},
-#         room=room,
-#         # include_self=False,
-#     )
-
 @socket.on("join-chat")
 def join_private_chat(data):
+    print()
     room = data["rid"]
-    current_user_email = session["user"]
-    random_email = User.get_random_emails(exclude_email=current_user_email, num_emails=1)
+    join_room(room=room)
+    socket.emit(
+        "joined-chat",
+        {"msg": f"{room} is now online."},
+        room=room,
+        # include_self=False,
+    )
 
-    if random_email:
-        join_room(room=room)
-        socket.emit(
-            "joined-chat",
-            {"msg": f"{room} is now online and connected with {random_email[0]}."},
-            room=room,
-        )
-    else:
-        socket.emit(
-            "error",
-            {"msg": "No other users available to connect."},
-            room=room,
-        )
+# @socket.on("join-chat")
+# def join_private_chat(data):
+#     room = data["rid"]
+#     current_user_email = session["user"]
+#     random_email = User.get_random_emails(exclude_email=current_user_email, num_emails=1)
+
+#     if random_email:
+#         join_room(room=room)
+#         socket.emit(
+#             "joined-chat",
+#             {"msg": f"{room} is now online and connected with {random_email[0]}."},
+#             room=room,
+#         )
+#     else:
+#         socket.emit(
+#             "error",
+#             {"msg": "No other users available to connect."},
+#             room=room,
+#         )
 
 
 # Outgoing event handler
